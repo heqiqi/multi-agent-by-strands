90e6f580df26517171e11c6d35a99124f9e0adee
He Qi <qihen@amazon.com>
2025-06-08 00:07:15 +0800

    add some useful mcp tools

---
+++ tools/aws_mcp.py
@@ -0,0 +1,49 @@
+from strands.tools.mcp import MCPClient
+from mcp import stdio_client, StdioServerParameters
+
+aws_docs_client = MCPClient(
+    lambda: stdio_client(StdioServerParameters(command="uvx", args=["awslabs.aws-documentation-mcp-server@latest"]))
+)
+
+# {
+#   "mcpServers": {
+#     "awslabs.cost-analysis-mcp-server": {
+#       "command": "uvx",
+#       "args": ["awslabs.cost-analysis-mcp-server@latest"],
+#       "env": {
+#         "FASTMCP_LOG_LEVEL": "ERROR",
+#         "AWS_PROFILE": "your-aws-profile"
+#       },
+#       "disabled": false,
+#       "autoApprove": []
+#     }
+#   }
+# }
+
+aws_cost_client = MCPClient(
+    lambda: stdio_client(StdioServerParameters(command="uvx", args=["awslabs.cost-analysis-mcp-server@latest"], env={ "FASTMCP_LOG_LEVEL": "ERROR"}))
+)
+
+# {
+#   "mcpServers": {
+#     "awslabs.cloudwatch-logs-mcp-server": {
+#       "autoApprove": [],
+#       "disabled": false,
+#       "timeout": 60,
+#       "command": "uvx",
+#       "args": [
+#         "awslabs.cloudwatch-logs-mcp-server@latest",
+#       ],
+#       "env": {
+#         "AWS_PROFILE": "[The AWS Profile Name to use for AWS access]",
+#         "AWS_REGION": "[The AWS region to run in]",
+#         "FASTMCP_LOG_LEVEL": "ERROR"
+#       },
+#       "transportType": "stdio"
+#     }
+#   }
+# }
+
+aws_cloudwatch_logs_client = MCPClient(
+    lambda: stdio_client(StdioServerParameters(command="uvx", args=["awslabs.cloudwatch-logs-mcp-server@latest"], env={ "FASTMCP_LOG_LEVEL": "ERROR", "AWS_REGION": "us-east-1"}))
+)
